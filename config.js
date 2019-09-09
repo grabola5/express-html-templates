ab774fc8c9533bac1dab1be9b49e81ac84542ee2
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-09 12:11:14 +0200

    Use Google API

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '1089880928399-65co8vuiv56v7o18i61rhan60312sn1h.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'WQjAIlZFPSeREqIsLhGn4Ezq',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
